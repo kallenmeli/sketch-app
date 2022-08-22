5be5a9bf2c2038ac23521efc0ff3d55de34e2906
Kevin Allen <kevinallenlf@gmail.com>
2022-08-22 20:36:24 -0300

    tests: created tests for draw methods

--- internal/canvas/draw_test.go
+++ internal/canvas/draw_test.go
@@ -225,3 +225,97 @@ func TestDrawRequest_IsLastRow(t *testing.T) {
 		})
 	}
 }
+
+func TestDrawRequest_IsFirstRow(t *testing.T) {
+	testCases := []struct {
+		name       string
+		row        int
+		y          int
+		isFirstRow bool
+	}{
+		{
+			name:       "when row is the same value of Y coordinate, should return true",
+			y:          0,
+			row:        0,
+			isFirstRow: true,
+		},
+		{
+			name:       "when row is lower than Y coordinate, should return false",
+			y:          5,
+			row:        1,
+			isFirstRow: false,
+		},
+		{
+			name:       "when row is greater than Y coordinate, should return false",
+			y:          0,
+			row:        1,
+			isFirstRow: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			request := faker.NewSingleDrawRequest(t)
+			request.Y = tc.y
+			got := request.IsFirstRow(tc.row)
+			expected := tc.isFirstRow
+			assert.Equal(t, expected, got)
+		})
+	}
+}
+
+func TestDrawRequest_IsLateralOutline(t *testing.T) {
+	testCases := []struct {
+		name     string
+		colIndex int
+		x        int
+		width    int
+		expected bool
+	}{
+		{
+			name:     "when colIndex is in the first colIndex after the X coordinate, should return true",
+			colIndex: 0,
+			x:        0,
+			expected: true,
+		},
+		{
+			name:     "when colIndex is before the X coordinate, should return false",
+			colIndex: 0,
+			x:        5,
+			expected: false,
+		},
+		{
+			name:     "when colIndex is after the X coordinate and before the last colIndex, should return false",
+			colIndex: 2,
+			x:        0,
+			width:    4,
+			expected: false,
+		},
+		{
+			name:     "when colIndex is the last column, should return true",
+			colIndex: 3,
+			x:        0,
+			width:    4,
+			expected: true,
+		},
+		{
+			name:     "when colIndex + x coordinate is the last column, should return true",
+			colIndex: 4,
+			x:        2,
+			width:    3,
+			expected: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			request := faker.NewSingleDrawRequest(t)
+			request.X = tc.x
+			request.Width = tc.width
+			got := request.IsLateralOutline(tc.colIndex)
+			expected := tc.expected
+
+			assert.Equal(t, expected, got)
+		})
+	}
+}
